ab34d2f3d66f3d19876914703ac1f403be38ae1c
toyboot4e <toyboot4e@gmail.com>
2022-06-08 07:59:14 +0900

    add P05.hs

---
+++ app/P05.hs
@@ -0,0 +1,14 @@
+module Main where
+
+sumDigitsRec :: Int -> Int -> Int
+sumDigitsRec 0 sum = sum
+sumDigitsRec x sum = sumDigitsRec (x `div` 10) (sum + x `rem` 10)
+
+sumDigits :: Int -> Int
+sumDigits x = sumDigitsRec x 0
+
+main :: IO ()
+main = do
+  [n, a, b] <- map read . words <$> getLine
+  let xs = [x | x <- [1 .. n], let s = sumDigits x, a <= s && s <= b]
+  print $ sum xs
