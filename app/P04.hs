a62331e66804c417f1a0a0ec444690959cc056e6
toyboot4e <toyboot4e@gmail.com>
2022-06-07 23:08:39 +0900

    add P04.hs

---
+++ app/P04.hs
@@ -0,0 +1,17 @@
+module Main where
+
+main :: IO ()
+main = do
+  n500 <- read <$> getLine :: IO Int
+  n100 <- read <$> getLine :: IO Int
+  n50 <- read <$> getLine :: IO Int
+  total <- read <$> getLine :: IO Int
+
+  print $
+    sum
+      [ 1
+        | x500 <- [0 .. n500],
+          x100 <- [0 .. n100],
+          x50 <- [0 .. n50],
+          500 * x500 + 100 * x100 + 50 * x50 == total
+      ]
